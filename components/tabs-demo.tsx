be4633909bf4d61f3e3aab0690f93ad20d010dbc
haris-khandre <harishkhandre2901@gmail.com>
2024-05-27 13:07:29 +0530

    done with mobile design

--- components/tabs-demo.tsx
+++ components/tabs-demo.tsx
@@ -36,7 +36,7 @@ export function TabsDemo() {
   ];
 
   return (
-    <div className="h-[20rem] md:h-[30rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start my-12">
+    <div className="h-[40rem] md:h-[30rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start my-12 px-8">
       <Tabs tabs={tabs} />
     </div>
   );
